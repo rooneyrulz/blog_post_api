8e6a356cdf9764497fc7d2bf7cd6b65d2fca9ebc
unknown <ruzny@ruzny.com>
2019-12-04 12:18:36 +0530

    schemas setup & establishing mongo connection

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const {
     createServer
 } = require('http');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = createServer(app);
 
-app.use('', (req, res, next) => res.status(200).send('It works!'));
+app.use('', require('./routes'));
+
+async function init() {
+    try {
+        const con = await mongoose.connect('mongodb://localhost:27017/blog_post_api', {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: true
+        })
+        if (con) console.log('connecting to mongodb!')
+    } catch (error) {
+        throw error.message
+    }
+}
+
+init()
 
 server.listen(process.env.PORT || 5000, () => console.log(`server running on port ${process.env.PORT || 5000}!`));
